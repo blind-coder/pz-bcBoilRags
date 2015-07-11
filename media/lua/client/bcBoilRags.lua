77e26865e887628ca0651ba0e1a689c4f9abfcb6
Benjamin 'blindCoder' Schieder <benjamin@anderdonau.de>
2015-07-11 10:04:14 +0200

    code cleanup

--- media/lua/client/bcBoilRags.lua
+++ media/lua/client/bcBoilRags.lua
@@ -1,33 +1,3 @@
-bcBoilRags = {};
-
-function bcBoilRags.takeRags(player, item)
-	player = getSpecificPlayer(player);
-	local inv = player:getInventory();
-
-	for x=1,4 do
-		if item:getFullType() == "bcBoilRags.PotOfCleanRags" then
-			inv:AddItem("Base.AlcoholRippedSheets");
-		else
-			inv:AddItem("Base.RippedSheetsDirty");
-		end
-	end
-	inv:AddItem("Base.Pot");
-
-	inv:Remove(item);
+function TakeCleanRags_OnCreate(items, result, player)
+	player:getInventory():AddItem("Base.Pot"); -- recipes can not give you two different items, so work around this
 end
-
-function bcBoilRags.createInventoryMenu(player, context, items)
-	item = items[1];
-	if not instanceof(item, "InventoryItem") then
-		item = item.items[1];
-	end
-	if item == nil then return end;
-
-	if item:getFullType() == "bcBoilRags.PotOfCleanRags" then
-		context:addOption("Take clean rags", player, bcBoilRags.takeRags, item);
-	elseif item:getFullType() == "bcBoilRags.PotOfDirtyRags" then
-		context:addOption("Take dirty rags", player, bcBoilRags.takeRags, item);
-	end
-end
-
-Events.OnFillInventoryObjectContextMenu.Add(bcBoilRags.createInventoryMenu);
